cb23ac88a9aea5357b2e3a82264a02865637cedf
0xbea <19748276+0xbea@users.noreply.github.com>
2025-06-21 15:23:28 +0200

    generate report

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from stats import count_words
+from stats import count_characters
+from stats import sort_chars
 
 
 def get_book_text(filepath):
@@ -8,8 +10,18 @@ def get_book_text(filepath):
 
 
 def main(relative_path):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {relative_path}...")
     num_words = count_words(get_book_text(relative_path))
-    print(f"{num_words} words found in the document")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    num_characters = sort_chars(count_characters(get_book_text(relative_path)))
+    for dict in num_characters:
+        # print(dict)
+        if dict["char"].isalpha():
+            print(f"{dict['char']}: {dict['num']}")
+    # print(num_characters)
 
 
 main("books/frankenstein.txt")
--- stats.py
+++ stats.py
@@ -1,2 +1,28 @@
 def count_words(text):
     return len(text.split())
+
+
+# takes the text from the book as a string, and returns the number of times each character, (including symbols and spaces), appears in the string.
+def count_characters(text):
+    counts = {}
+    for char in text.lower():
+        if char in counts:
+            counts[char] += 1
+        else:
+            counts[char] = 1
+    return counts
+
+
+def sort_on(items):
+    return items["num"]
+
+
+# takes the dictionary of characters and their counts and returns a sorted list of dictionaries.
+# Each dictionary should have two key-value pairs: one for the character itself and one for that character's count (e.g. {"char": "b", "num": 4868}).
+def sort_chars(dict):
+    char_count_list = []
+    for char in dict:
+        char_count_list.append({"char": char, "num": dict[char]})
+    char_count_list.sort(reverse=True, key=sort_on)
+    # print(char_count_list)
+    return char_count_list
